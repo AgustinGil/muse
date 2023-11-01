d98f9f43607bddf863cba6d2e7bb794411a4b4d3
sfermin9731 <sfermin.9731@unimar.edu.ve>
2023-10-31 23:17:14 -0400

    Eliminar todos los archivos y carpetas

--- config/conexion.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $servidor = 'localhost';
-    $usuario = 'root';
-    $contraseña = '';
-    $bDatos = 'bdmuse';
-
-    $conexion = mysqli_connect($servidor, $usuario, $contraseña, $bDatos) or die(mysql_error($mysqli));
-?>
