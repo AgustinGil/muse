743e97fbf2e42c61ffe6573b227c34f8d6c9535b
sfermin9731 <sfermin.9731@unimar.edu.ve>
2023-10-31 22:38:45 -0400

    actualizar los archivos y conectatlos con --include--

--- registro.php
+++ registro.php
@@ -44,9 +44,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 
+    <link rel="stylesheet" type="text/css" href="css/normalize.css">
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <link rel="stylesheet" type="text/css" href="css/login.css">
-    <link rel="stylesheet" type="text/css" href="css/footer.css">
     <script src="js/script.js"></script>
     <link rel="icon" href="assets/img/icons/favicon.ico">
     <title>Muse</title>
@@ -166,15 +166,7 @@
         </div>
     </main>
 
-    <hr id="barra-footer">
-    <footer>
-        <div id="footer__logo"><img src="assets/img/icons/imagotipo.svg"></div>
-        <p id="derechos-footer">Copyright ©2023 Muse. <br>Developed by Fermin S. , Gil A. , Jiménez G. </p>
-        <div id="footer__iconos">
-            <img class="icono-footer filtro-blanco" src="assets/img/icons/instagram.svg">
-            <img class="icono-footer filtro-blanco" src="assets/img/icons/twitter.svg">
-            <img class="icono-footer filtro-blanco" src="assets/img/icons/facebook-f.svg">
-        </div>
-</footer>
+    <!-- Footer de la Pagina -->
+    <?php include('includes/footer.php') ?>
 </body>
 </html>
